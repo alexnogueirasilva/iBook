35fc814b4894338403c4c40bc59e54ae5860003f
Alex Nogueira <alex@devaction.com.br>
2021-09-07 17:14:14 -0300

    create template HomeTemplate

---
+++ components/organisms/Releases.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class='releases'></div>
+</template>
+
+<script>
+export default {
+  name: 'Releases'
+}
+</script>
+
+<style lang='scss' scoped>
+.releases {
+  display: grid;
+  grid-gap: 1.3rem;
+
+  h2 {
+    font-size: 1.375rem;
+    font-weight: 500;
+    color: color(dark, darkest);
+  }
+}
+</style>
