6ef66972851bbeb0345cc37cb1bd2aec76fc9588
Alex Nogueira <alex@devaction.com.br>
2021-09-11 12:18:58 -0300

    correcting bugs in iBook npm

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -21,7 +21,7 @@ export default {
   ],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
-  plugins: [],
+  plugins: ['@/plugins/accessor'],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
   components:  [{ path: '@/components', pathPrefix: false }],
@@ -35,7 +35,7 @@ export default {
   ],
 
   // Modules: https://go.nuxtjs.dev/config-modules
-  modules: [],
+  modules: ['@nuxtjs/axios'],
 
   styleResources: {
     // your settings here
--- pages/index.vue
+++ pages/index.vue
@@ -1,15 +1,19 @@
 <template>
-  <div class="box">
-    <HomeTemplate/>
+  <div class='box'>
+    <HomeTemplate />
   </div>
 </template>
 
 <script lang='ts'>
 import Vue from 'vue'
 import HomeTemplate from '~/components/templates/HomeTemplate.vue'
+import { books } from '@/store'
 
- export default Vue.extend({
-   components: { HomeTemplate },
-   layout: 'ibook'
- })
+export default Vue.extend({
+  components: { HomeTemplate },
+  layout: 'ibook',
+  asyncData(){
+    console.log(books.books)
+  }
+})
 </script>
